1048ed9d68a7413d99319a49d1cedcc5a991b067
Lientur03 <andreslientur03@gmail.com>
2025-05-23 16:35:58 -0400

    Desarrollo Honesto en Clase (23-05) | Programación

---
+++ Condicionales/Ejercicios/Ejercicio_5.py
@@ -0,0 +1,44 @@
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+
+# table.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+# table.add_rows(
+#     [
+#         ["Adelaide", 1295, 1158259, 600.5],
+#         ["Brisbane", 5905, 1857594, 1146.4],
+#         ["Darwin", 112, 120900, 1714.7],
+#         ["Hobart", 1357, 205556, 619.5],
+#         ["Sydney", 2058, 4336374, 1214.8],
+#         ["Melbourne", 1566, 3806092, 646.9],
+#         ["Perth", 5386, 1554769, 869.4],
+#     ]
+# )
+# print(table)
+
+# animales = [
+#     # Elemento 1, Índice 0
+#     ['mamíferos',['Vaca Lechera', 'Hammster Dorado', 'Pony Salvaje', 'Chanco con Chaleco']],
+
+#     # Elemento 2, Índice 1
+#     ['reptiles',['Cobra Kai', 'Balano', 'Dragón de Komodo', 'Mamba Negra']],
+
+#     # Elemento 3, Índice 2
+#     ['aves', ['pato', 'pinguíno', 'canario', 'gallina']]
+# ]
+
+# for i in range(len(animales)):
+#     print(animales[i][0])
+
+lista_comidas = ['Lasaña', 'Pollo Arvejado', 'Curanto', 'Sopaipillas Pasadas']
+# 1.- Imprimir la cantidad de elementos de la lista
+for elementos in lista_comidas:
+    print(elementos)
+
+# 2.- Imprimir la lista completa de comidas
+print(lista_comidas)
+
+# 3.- Imprimir todas las comidas una a una
+
+
+# 4.- Preguntar al usuario qué comida busca, número = índice
