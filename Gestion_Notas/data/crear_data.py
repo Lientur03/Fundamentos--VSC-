68140157daa0a1bcf6256b0aced83452f2a02f1e
Lientur03 <andreslientur03@gmail.com>
2025-06-12 09:54:08 -0400

    Archivos Al Día, después de que mi problema estaba en mi propia carpeta Fundamentos (VSC)

--- Gestion_Notas/data/crear_data.py
+++ Gestion_Notas/data/crear_data.py
@@ -2,7 +2,7 @@ import os
 
 def guardar_data(nombre_lista, lista_data, nombre_archivo):
     archivo_datos = nombre_archivo
-    ubicacion_archivo = os.path.join('gestion_notas/data', archivo_datos)
+    ubicacion_archivo = os.path.join('Gestion_Notas/data', archivo_datos)
     ubicacion_archivo = os.path.abspath(ubicacion_archivo)
     ubicacion_archivo = os.path.realpath(ubicacion_archivo)    
     archivo = open(f"{ubicacion_archivo}", "w+")
