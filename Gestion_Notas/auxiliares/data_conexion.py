ed43e11dd9e0f8668ed71556b454ae38ff94e523
Lientur03 <andreslientur03@gmail.com>
2025-07-03 16:58:25 -0400

    SAMM y SDL (03-07) Clase Presencial

---
+++ Gestion_Notas/auxiliares/data_conexion.py
@@ -0,0 +1,5 @@
+servidor = 'localhost'
+puerto = 3307
+usuario = 'root'
+base_datos = 'gestion_notas'
+contrasena = ''
--- Gestion_Notas/data/conexion.py
+++ Gestion_Notas/data/conexion.py
@@ -1,22 +1,29 @@
 import mysql.connector
 from mysql.connector import errorcode
-from auxiliares.data_conexion import servidor, puerto, usuario, base_datos, contraseña
-try:
-    conexion = mysql.connector.connect(
-        host = servidor,
-        port = puerto,
-        user = usuario,
-        database = base_datos,
-        password = contraseña
-    )
+from auxiliares.data_conexion import servidor, puerto, usuario, base_datos, contrasena
 
-    if conexion and conexion.is_connected():
-        cursor = conexion.cursor()
+def conectar_db():
+    try:
+        conexion = mysql.connector.connect(
+            host = servidor,
+            port = puerto,
+            user = usuario,
+            database = base_datos,
+            password = contrasena
+        )
 
-        cursor.execute('SELECT * FROM asignaturas')
-        resultado = cursor.fetchall()
-        print(resultado)
-    else:
-        print('No se ha podido establecer conexión con la base de datos')
-except:
-    print('No se ha podido establecer conexión con el servidor')
+        if conexion and conexion.is_connected():
+            cursor = conexion.cursor()
+
+            cursor.execute('SELECT * FROM asignaturas')
+            resultado = cursor.fetchall()
+            print(resultado)
+        else:
+            print('No se ha podido establecer conexión con la base de datos')
+    except mysql.connector.Error as error_conexion:
+        if error_conexion.errno == errorcode.ER_ACCESS_DENIED_CHANGE_USER_ERROR:
+            print('Credenciales de acceso incorrectas')
+        elif error_conexion.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Base de datos incorrecta o no existe')
+        else:
+            print(error_conexion)
