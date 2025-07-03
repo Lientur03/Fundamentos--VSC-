8ce79db2eadc2ac8ec2101e90ec0ec3e11680d66
Lientur03 <andreslientur03@gmail.com>
2025-07-03 16:12:15 -0400

    Nuevos Métodos en SAMM y SDL. (03-07)

--- Condicionales/Ejercicios/test.py
+++ Condicionales/Ejercicios/test.py
@@ -1,13 +1,17 @@
 while True:
     try:
-        numerador = int(input('Ingrese el numerador: '))
-        denominador = int(input('Ingrese el denominador: '))
+        numerador = float(input('Ingrese el numerador: '))
+        denominador = float(input('Ingrese el denominador: '))
 
         division = numerador/denominador
         print(f'{numerador} / {denominador} = {division} ')
 
-        respuesta = input('Desea continuar ¿Sí o no?')
-        if respuesta.lower() == 'no':
+        respuesta = input('Desea continuar ¿Sí o no?: ')
+        if respuesta.lower() == 'no' or respuesta.lower() == 'n':
             break
-    except:
+    except ValueError:
         print("El número no corresponde a entero")
+    except ZeroDivisionError:
+        print("No se puede dividir por 0")
+    except:
+        print("No se puede realizar la operación")
--- Gestion_Notas/data/conexion.py
+++ Gestion_Notas/data/conexion.py
@@ -1,14 +1,22 @@
 import mysql.connector
+from mysql.connector import errorcode
+from auxiliares.data_conexion import servidor, puerto, usuario, base_datos, contraseña
+try:
+    conexion = mysql.connector.connect(
+        host = servidor,
+        port = puerto,
+        user = usuario,
+        database = base_datos,
+        password = contraseña
+    )
 
-conexion = mysql.connector.connect(
-    host='localhost',
-    port=3308,
-    user='root',
-    database='gestion_notas'
-)
+    if conexion and conexion.is_connected():
+        cursor = conexion.cursor()
 
-cursor = conexion.cursor()
-
-cursor.execute('SELECT * FROM docentes')
-resultado = cursor.fetchall()
-print(resultado)
+        cursor.execute('SELECT * FROM asignaturas')
+        resultado = cursor.fetchall()
+        print(resultado)
+    else:
+        print('No se ha podido establecer conexión con la base de datos')
+except:
+    print('No se ha podido establecer conexión con el servidor')
