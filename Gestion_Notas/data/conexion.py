126ca42b5895563f635006906c68fbc57b6b7f12
Lientur03 <andreslientur03@gmail.com>
2025-07-04 16:09:38 -0400

    Modificaciones Sin Terminar [04-07] Clase Presencial de Programación.

--- Gestion_Notas/data/conexion.py
+++ Gestion_Notas/data/conexion.py
@@ -1,8 +1,8 @@
 import mysql.connector
 from mysql.connector import errorcode
-from auxiliares.data_conexion import servidor, puerto, usuario, base_datos, contrasena
+from auxiliares.data_conexion import servidor,puerto,usuario,base_datos,contrasena
 
-def conectar_db():
+def conectar_db(consulta):
     try:
         conexion = mysql.connector.connect(
             host = servidor,
@@ -12,16 +12,16 @@ def conectar_db():
             password = contrasena
         )
 
-        if conexion and conexion.is_connected():
+        if conexion  and conexion.is_connected():
             cursor = conexion.cursor()
 
-            cursor.execute('SELECT * FROM asignaturas')
+            cursor.execute(consulta)
             resultado = cursor.fetchall()
-            print(resultado)
+            return resultado
         else:
             print('No se ha podido establecer conexión con la base de datos')
     except mysql.connector.Error as error_conexion:
-        if error_conexion.errno == errorcode.ER_ACCESS_DENIED_CHANGE_USER_ERROR:
+        if error_conexion.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print('Credenciales de acceso incorrectas')
         elif error_conexion.errno == errorcode.ER_BAD_DB_ERROR:
             print('Base de datos incorrecta o no existe')
--- Gestion_Notas/principal.py
+++ Gestion_Notas/principal.py
@@ -1,45 +1,33 @@
-from negocio.negocio_asignaturas import obtener_listado_asignaturas, guardar_nueva_asignatura, actualizar_asignatura, eliminar_asignatura
-from negocio.negocio_docentes import obtener_listado_docentes, guardar_nuevo_docente, actualizar_docente, eliminar_docente
+from negocio.negocio_asignaturas import obtener_listado_asignaturas,guardar_nueva_asignatura,actualizar_asignatura,eliminar_asignatura
+from negocio.negocio_docentes import obtener_listado_docentes, guardar_nuevo_docente,actualizar_docente,eliminar_docente
 from auxiliares.version import version_actual
 from data.conexion import conectar_db
+from prettytable import PrettyTable
 
-def menu_principal():
+def cargar_menu(tipo_menu):
     print()
     print(f"Sistema Gestión Notas v.{version_actual}")
     print("==============================")
-    print("[1] Gestión de Asignaturas")
-    print("[2] Gestión de Docentes")
-    print("[3] Conectar a DB")
-    print("[0] Salir")
-    print()
-
-def sub_menu_asignaturas():
-    print()
-    print("Gestión Asignaturas")
-    print("===================")
-    print("[1] Listado de Asignaturas")
-    print("[2] Agregar Asignatura")
-    print("[3] Modificar Asignatura")
-    print("[4] Eliminar Asignatura")
-    print("[0] Volver al menú principal")
-    print()
-
-def sub_menu_docentes():
-    print()
-    print("Gestión Docentes")
-    print("================")
-    print("[1] Listado de Docentes")
-    print("[2] Agregar Docente")
-    print("[3] Modificar Docente")
-    print("[4] Eliminar Docente")
-    print("[0] Volver al menú principal")
-    print()
+    consulta = f'''
+        SELECT numero_opcion,opcion_menu FROM opciones_menu
+        WHERE tipo_menu = {tipo_menu}
+    '''
+    resultado = conectar_db(consulta)
+    tabla_menu = PrettyTable()
+    tabla_menu.field_names = ['N°', 'Opción']
+    contador = 0
+    if resultado != None:
+        for asignatura in resultado:
+            contador += 1
+            tabla_menu.add_row(asignatura)
+    print(tabla_menu)
+    opcion_menu = input(f"Seleccione su Opción [0-{contador}]: ")
+    return opcion_menu
 
 def ejecucion_principal():
     while True:
-        menu_principal()
-        opcion_menu = input("Seleccione su Opción [0-1]: ")
-        
+        opcion_menu = cargar_menu()
+
         if opcion_menu == "1":
             sub_menu_asignaturas()
             opcion_submenu_asignaturas = input("Seleccione su Opción [0-4]: ")
@@ -75,9 +63,7 @@ def ejecucion_principal():
             else:
                 print("Opción Inválida, vuelva a ingresar...")
                 return
-        elif opcion_menu == "3":
-            conectar_db()
-
+            
         elif opcion_menu == "0":
             break
         else:
