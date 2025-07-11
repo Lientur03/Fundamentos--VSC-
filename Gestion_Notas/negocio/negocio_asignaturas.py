596bd44e159a325d4f4b3302d65b44e9fc24f142
Lientur03 <andreslientur03@gmail.com>
2025-07-11 14:53:49 -0400

    Muchos cambios en la clase presencial de ayer (10-07-2025) Programación en 3° Oportunidad

--- Gestion_Notas/negocio/negocio_asignaturas.py
+++ Gestion_Notas/negocio/negocio_asignaturas.py
@@ -1,53 +1,73 @@
 from data.crear_data import guardar_data
 from data.leer_data import listado_data, obtener_indice_data
 from data.asignaturas import asignaturas
+from data.conexion import leer_datos, insertar_datos
+from prettytable import PrettyTable
 
 # READ DATA
-def obtener_listado_asignaturas():    
+
+
+def obtener_listado_asignaturas():
     print()
     print('Listado de Asignaturas')
     print('======================')
-    lista = listado_data('asignaturas.py')
-    if len(lista) > 0:
-        contador = 0
-        for data in sorted(lista):
-            contador += 1
-            print(f'[{contador}] {data}')
+    consulta = f'''
+        SELECT id_asignatura,codigo_asig,nombre_asig 
+        FROM asignaturas
+        WHERE habilitado = 1
+    '''
+    tabla_asignaturas = PrettyTable()
+    tabla_asignaturas.field_names = [
+        'N°', 'Código Asignatura', 'Nombre Asignatura']
+    listado_asignaturas = leer_datos(consulta)
+    if listado_asignaturas != None:
+        for asignatura in listado_asignaturas:
+            tabla_asignaturas.add_row(asignatura)  # type: ignore
     else:
         print('No se han encontrado datos.')
-
-# READ DATA
-# def buscar_asignatura():
-#     asignatura_encontrada = 'asignatura NO encontrada'
-#     busqueda = input("Ingrese asignatura a buscar: ")
-#     for asignatura in asignaturas:
-#         if busqueda.lower() in asignatura.lower():
-#             asignatura_encontrada = asignatura
-#     return asignatura_encontrada
+    print(tabla_asignaturas)
 
 # CREATE DATA
+
+
 def guardar_nueva_asignatura():
     obtener_listado_asignaturas()
-    nueva_asignatura = input('Ingrese nueva asignatura: ')
-    asignaturas.append(nueva_asignatura.title())
-    mensaje = guardar_data('asignaturas', asignaturas, 'asignaturas.py')
-    print(f'{mensaje} de asignatura {nueva_asignatura}')
-    
+    cod_asignatura = input('Ingrese código nueva asignatura: ')
+    nombre_asignatura = input('Ingrese nombre nueva asignatura: ')
+    desc_asignatura = input('Ingrese descripción nueva asignatura: ')
+
+    if cod_asignatura and nombre_asignatura != '':
+        consulta = '''
+            INSERT INTO asignaturas 
+            (codigo_asig,nombre_asig,descripcion_asig,habilitado) 
+            VALUES (%s,%s,%s,%s)
+        '''
+        valores = (cod_asignatura.upper(),
+                   nombre_asignatura.title(), desc_asignatura, 1)
+        mensaje = insertar_datos(consulta, valores)
+        print(f'{mensaje}')
+    else:
+        print('El código y el nombre de la asignatura son campos obligatorios.')
+
 # UPDATE DATA
+
+
 def actualizar_asignatura():
     obtener_listado_asignaturas()
     busqueda = input("Ingrese asignatura a buscar: ")
     indice_asignatura = obtener_indice_data('asignaturas.py', busqueda)
-    
+
     if indice_asignatura is not None:
-        asignatura_modificada = input("Ingrese nuevo nombre de asignatura")
+        asignatura_modificada = input("Ingrese nuevo nombre de asignatura: ")
         asignaturas[indice_asignatura] = asignatura_modificada.title()
         mensaje = guardar_data('asignaturas', asignaturas, 'asignaturas.py')
         print(f'{mensaje} de asignatura {asignatura_modificada}')
     else:
-        print("Asignatura NO Encontrada")
+        print('Asignatura NO encontrada')
 
 # DELETE DATA
+
+
 def eliminar_asignatura():
     obtener_listado_asignaturas()
     busqueda = input("Ingrese asignatura a buscar: ")
@@ -59,4 +79,4 @@ def eliminar_asignatura():
         mensaje = guardar_data('asignaturas', asignaturas, 'asignaturas.py')
         print(f'{mensaje}, asignatura {asignatura} eliminada.')
     else:
-        print("Asignatura NO Encontrada")
+        print('Asignatura NO encontrada')
--- Gestion_Notas/principal.py
+++ Gestion_Notas/principal.py
@@ -1,10 +1,12 @@
-from negocio.negocio_asignaturas import obtener_listado_asignaturas,guardar_nueva_asignatura,actualizar_asignatura,eliminar_asignatura
-from negocio.negocio_docentes import obtener_listado_docentes, guardar_nuevo_docente,actualizar_docente,eliminar_docente
+from negocio.negocio_asignaturas import obtener_listado_asignaturas, guardar_nueva_asignatura, actualizar_asignatura, eliminar_asignatura
+from negocio.negocio_docentes import obtener_listado_docentes, guardar_nuevo_docente, actualizar_docente, eliminar_docente
 from auxiliares.version import version_actual
-from data.conexion import conectar_db
+from data.conexion import leer_datos
+import auxiliares.mensajes_sistema as mensajes
 from prettytable import PrettyTable
 
-def cargar_menu(tipo_menu):
+
+def cargar_menu(tipo_menu, titulo_menu):
     print()
     print(f"Sistema Gestión Notas v.{version_actual}")
     print("==============================")
@@ -12,61 +14,62 @@ def cargar_menu(tipo_menu):
         SELECT numero_opcion,opcion_menu FROM opciones_menu
         WHERE tipo_menu = {tipo_menu}
     '''
-    resultado = conectar_db(consulta)
+    resultado = leer_datos(consulta)
     tabla_menu = PrettyTable()
-    tabla_menu.field_names = ['N°', 'Opción']
+    tabla_menu.field_names = ['N°', f'{titulo_menu}']
     contador = 0
     if resultado != None:
         for asignatura in resultado:
             contador += 1
-            tabla_menu.add_row(asignatura)
+            tabla_menu.add_row(asignatura)  # type: ignore
     print(tabla_menu)
-    opcion_menu = input(f"Seleccione su Opción [0-{contador}]: ")
+    opcion_menu = input(f"Seleccione su Opción [0-{contador-1}]: ")
     return opcion_menu
+
 
 def ejecucion_principal():
     while True:
-        opcion_menu = cargar_menu()
+        opcion_menu = cargar_menu(1, 'Menú Principal')
 
         if opcion_menu == "1":
-            sub_menu_asignaturas()
-            opcion_submenu_asignaturas = input("Seleccione su Opción [0-4]: ")
-            
-            if opcion_submenu_asignaturas == "1":
-                obtener_listado_asignaturas()
-            elif opcion_submenu_asignaturas == "2":
-                guardar_nueva_asignatura()
-            elif opcion_submenu_asignaturas == "3":
-                actualizar_asignatura()
-            elif opcion_submenu_asignaturas == "4":
-                eliminar_asignatura()
-            elif opcion_submenu_asignaturas == "0":
-                return
-            else:
-                print("Opción Inválida, vuelva a ingresar...")
-                return
-            
-        elif opcion_menu == "2":
-            sub_menu_docentes()
-            opcion_submenu_docentes = input("Seleccione su Opción [0-4]: ")
+            while True:
+                opcion_submenu = cargar_menu(2, 'Gestión Asignaturas')
+                if opcion_submenu == "1":
+                    obtener_listado_asignaturas()
+                elif opcion_submenu == "2":
+                    guardar_nueva_asignatura()
+                elif opcion_submenu == "3":
+                    actualizar_asignatura()
+                elif opcion_submenu == "4":
+                    eliminar_asignatura()
+                elif opcion_submenu == "0":
+                    print(f'{mensajes.volver}')
+                    break
+                else:
+                    print(f'{mensajes.invalido}')
 
-            if opcion_submenu_docentes == "1":
-                obtener_listado_docentes()
-            elif opcion_submenu_docentes == "2":
-                guardar_nuevo_docente()
-            elif opcion_submenu_docentes == "3":
-                actualizar_docente()
-            elif opcion_submenu_docentes == "4":
-                eliminar_docente()
-            elif opcion_submenu_docentes == "0":
-                return
-            else:
-                print("Opción Inválida, vuelva a ingresar...")
-                return
-            
+        elif opcion_menu == "2":
+            while True:
+                opcion_submenu = cargar_menu(3, 'Gestión Docentes')
+                if opcion_submenu == "1":
+                    obtener_listado_docentes()
+                elif opcion_submenu == "2":
+                    guardar_nuevo_docente()
+                elif opcion_submenu == "3":
+                    actualizar_docente()
+                elif opcion_submenu == "4":
+                    eliminar_docente()
+                elif opcion_submenu == "0":
+                    print(f'{mensajes.volver}')
+                    break
+                else:
+                    print(f'{mensajes.invalido}')
+
         elif opcion_menu == "0":
+            print(f'{mensajes.salir}')
             break
         else:
-            print("Opción Inválida, vuelva a ingresar...")
+            print(f'{mensajes.invalido}')
+
 
 ejecucion_principal()
